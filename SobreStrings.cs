db8f02c67624d0f707b61de3597599ceabba4ca5
Vitor Castro <74258382+vitorcastroficial@users.noreply.github.com>
2025-07-26 21:01:07 -0300

    Create SobreStrings.cs

---
+++ SobreStrings.cs
@@ -0,0 +1,28 @@
+/*
+Métodos que adicionam espaços em branco para fins de formatação (PadLeft(), PadRight())
+Métodos que comparam duas cadeias de caracteres ou facilitam a comparação (Trim(), TrimStart(), TrimEnd(), GetHashcode(), a propriedade Length)
+Métodos que ajudam a determinar o que há dentro de uma cadeia de caracteres ou até mesmo recuperar apenas uma parte da cadeia de caracteres (Contains(), StartsWith(), EndsWith(), Substring())
+Métodos que alteram o conteúdo da cadeia de caracteres substituindo, inserindo ou removendo partes (Replace(), Insert(), Remove())
+Métodos que transformam uma cadeia de caracteres em uma matriz de cadeias de caracteres ou de caracteres (Split(), ToCharArray())
+*/
+
+// Segue variáveis e exemplos de uso
+string first = "Hello";
+string second = "World";
+
+// Formatação Composta:
+string result = string.Format("{0} {1}!", first, second);
+Console.WriteLine(result);
+
+// Interpolação de Caracteres:
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+
+// Moeda de Formatação (de acordo com o idioma selecionado no Windows pelo usuário):
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+// Formatação de Números:
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N} units");
